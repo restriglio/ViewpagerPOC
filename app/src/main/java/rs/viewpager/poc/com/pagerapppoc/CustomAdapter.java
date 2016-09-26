ad1204492fe33432fec37560ca897f30b0f59176
Raul Striglio <raul.striglio@gmail.com>
2016-09-26 12:29:09 -0300

    Added recyclerview implementation for MonthViewCalendar

---
+++ app/src/main/java/rs/viewpager/poc/com/pagerapppoc/CustomAdapter.java
@@ -0,0 +1,52 @@
+package rs.viewpager.poc.com.pagerapppoc;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by raul.striglio on 20/09/16.
+ */
+public class CustomAdapter extends RecyclerView.Adapter {
+
+    private Context mContext;
+    private List<String> mothsList;
+
+    public CustomAdapter(Context context, List<String> mothsList) {
+        mContext = context;
+        this.mothsList = mothsList;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_month_view, parent, false);
+        return new MonthViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        String month = mothsList.get(position);
+        ((MonthViewHolder) holder).titleTxt.setText(month);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mothsList.size();
+    }
+
+    public static class MonthViewHolder extends RecyclerView.ViewHolder {
+        public TextView titleTxt;
+        public MonthViewClanedar monthView;
+
+        public MonthViewHolder(View v) {
+            super(v);
+            this.titleTxt = (TextView) v.findViewById(R.id.titleTxt);
+            this.monthView = (MonthViewClanedar) v.findViewById(R.id.monthView);
+        }
+    }
+}
--- app/src/main/java/rs/viewpager/poc/com/pagerapppoc/MainActivity.java
+++ app/src/main/java/rs/viewpager/poc/com/pagerapppoc/MainActivity.java
@@ -2,36 +2,38 @@ package rs.viewpager.poc.com.pagerapppoc;
 
 import android.os.Bundle;
 import android.support.v4.view.ViewCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static ViewPager viewPager;
+    private static RecyclerView recyclerView;
+    private List<String> monthsList = Arrays.asList("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto");
+    private static LinearLayoutManager linearLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
-        viewPager.setAdapter(new CustomPagerAdapter(this));
-        ViewCompat.setAccessibilityDelegate(viewPager, new MyAccessibilityDelegateCompat());
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(new CustomAdapter(this, monthsList));
+        ViewCompat.setAccessibilityDelegate(recyclerView, new MyAccessibilityDelegateCompat());
 
     }
 
     public static void GotoNextPage() {
-        int page = viewPager.getCurrentItem();
-        if (page <= viewPager.getChildCount()) {
-            viewPager.setCurrentItem(page + 1, true);
-        }
+        recyclerView.getLayoutManager().scrollToPosition(linearLayoutManager.findLastVisibleItemPosition() + 1);
     }
 
     public static void GotoPrevPage() {
-        int page = viewPager.getCurrentItem();
-        if (page > 0) {
-            viewPager.setCurrentItem(page - 1, true);
-        }
+        recyclerView.getLayoutManager().scrollToPosition(linearLayoutManager.findFirstVisibleItemPosition() - 1);
     }
 
 }
